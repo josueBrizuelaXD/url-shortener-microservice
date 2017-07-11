e80d2a3deb7619e9c56a8851495b7feb8971442a
Josh <josue0991@icloud.com>
2017-07-10 20:28:47 -0500

    able to send index file

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
-	console.log("we connect to mongodb");
+	console.log("we connected to mongodb");
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, "views/index.html"));
+})
 
 
 
